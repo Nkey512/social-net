a16e038d47f6295b744b41da09ceaf0d901db3fd
Nikita Kuznetsov <nikita.kuznetsov512@gmail.com>
2021-04-05 22:29:53 +0300

    Add more logic to Users

--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -1,9 +1,14 @@
 const FOLLOW = 'FOLLOW';
 const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = 'SET_USERS';
+const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE';
+const SET_TOTAL_USERS_COUNT = 'SET_TOTAL_USERS_COUNT';
 
 let initialState = {
     users: [],
+    pageSize: 5,
+    totalUsersCount: 0,
+    currentPage: 1,
 }
 
 const usersReducer = (statePart = initialState, action) => {
@@ -34,14 +39,20 @@ const usersReducer = (statePart = initialState, action) => {
                 )
             }
         case SET_USERS:
-            return {...statePart, users: [...statePart.users, ...action.users]}
+            return {...statePart, users: action.users}
+        case SET_CURRENT_PAGE:
+            return {...statePart, currentPage: action.currentPage}
+        case SET_TOTAL_USERS_COUNT:
+            return {...statePart, totalUsersCount: action.count}
         default:
             return statePart;
     }
 }
 
-export const followAC = (userId) => ( {type: FOLLOW, userId: userId} )
-export const unfollowAC = (userId) => ( {type: UNFOLLOW, userId: userId} )
-export const setUsersAC = (users) => ( {type: SET_USERS, users: users} )
+export const followAC = (userId) => ( {type: FOLLOW, userId} )
+export const unfollowAC = (userId) => ( {type: UNFOLLOW, userId} )
+export const setUsersAC = (users) => ( {type: SET_USERS, users} )
+export const setCurrentPageAC = (currentPage) => ( {type: SET_CURRENT_PAGE, currentPage} )
+export const setTotalUsersCountAC = (totalUsersCount) => ( {type: SET_TOTAL_USERS_COUNT, count: totalUsersCount} )
 
 export default usersReducer;
