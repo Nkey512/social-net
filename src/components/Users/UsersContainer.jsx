a77a0df534f2d4c25ce2188038e0298d92428ecf
Nikita Kuznetsov <nikita.kuznetsov512@gmail.com>
2021-04-07 21:03:32 +0300

    Add preloader

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -3,7 +3,7 @@ import { followAC, setCurrentPageAC, setTotalUsersCountAC, setUsersAC, toggleIsF
 import * as axios from 'axios';
 import Users from './Users';
 import React from 'react';
-import preloader from './../../assets/images/preloader.gif'
+import Preloader from '../common/Preloader/Preloader';
 
 class UsersContainer extends React.Component {
 
@@ -30,7 +30,8 @@ class UsersContainer extends React.Component {
     render() {
         return (
             <>
-                { this.props.isFetching ? <img src={preloader}/> : null }
+                { this.props.isFetching ?
+                    <Preloader /> : null }
                 <Users
                     totalUsersCount={this.props.totalUsersCount}
                     pageSize={this.props.pageSize}
