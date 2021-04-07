96bfb7ef43cc89227823494a98f1eff2917d0f97
Nikita Kuznetsov <nikita.kuznetsov512@gmail.com>
2021-04-07 03:45:05 +0300

    Add preloader

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,8 +1,9 @@
-import { connect } from "react-redux";
+import { connect } from 'react-redux';
 import { followAC, setCurrentPageAC, setTotalUsersCountAC, setUsersAC, unfollowAC } from "../../redux/users-reducer";
 import * as axios from 'axios';
 import Users from './Users';
-import React from "react";
+import React from 'react';
+import preloader from './../../assets/images/preloader.gif'
 
 class UsersContainer extends React.Component {
 
@@ -24,14 +25,17 @@ class UsersContainer extends React.Component {
 
     render() {
         return (
-            <Users
-                totalUsersCount={this.props.totalUsersCount}
-                pageSize={this.props.pageSize}
-                currentPage={this.props.currentPage}
-                onPageChanged={this.onPageChanged}
-                users={this.props.users}
-                follow={this.props.follow}
-                unfollow={this.props.unfollow} />
+            <>
+                { this.props.isFetching ? <img src={preloader}/> : null }
+                <Users
+                    totalUsersCount={this.props.totalUsersCount}
+                    pageSize={this.props.pageSize}
+                    currentPage={this.props.currentPage}
+                    onPageChanged={this.onPageChanged}
+                    users={this.props.users}
+                    follow={this.props.follow}
+                    unfollow={this.props.unfollow} />
+            </>
         )
     }
 }
@@ -42,6 +46,7 @@ let mapStateToProps = (state) => {
         pageSize: state.usersPage.pageSize,
         totalUsersCount: state.usersPage.totalUsersCount,
         currentPage: state.usersPage.currentPage,
+        isFetching: state.usersPage.isFetching
     }
 }
 let mapDispatchToProps = (dispatch) => {
