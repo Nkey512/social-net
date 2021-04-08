ad69e9f2edbc6cead1637206627a7527aa554532
Nikita Kuznetsov <nikita.kuznetsov512@gmail.com>
2021-04-08 15:16:05 +0300

    Add ProfileContainer class component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import Header from './components/Header/Header';
-import Profile from './components/Profile/Profile';
 import DialogsContainer from './components/Dialogs/DialogsContainer'
 import News from './components/News/News';
 import Music from './components/Music/Music';
@@ -8,6 +7,7 @@ import Settings from './components/Settings/Settings';
 import { Route } from 'react-router';
 import NavbarContainer from './components/Navbar/NavbarContainer';
 import UsersContainer from './components/Users/UsersContainer';
+import ProfileContainer from './components/Profile/ProfileContainer';
 
 
 const App = (props) => {
@@ -18,7 +18,7 @@ const App = (props) => {
             <div className='app-wrapper-content'>
 
                 <Route path='/profile'
-                    render={() => <Profile />} />
+                    render={() => <ProfileContainer />} />
 
                 <Route path='/dialogs'
                     render={() => <DialogsContainer />} />
