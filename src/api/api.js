8f5014bf3df1e1fdcbae0b498f253a4967b7b170
Nikita Kuznetsov <nikita.kuznetsov512@gmail.com>
2021-04-12 02:14:24 +0300

    Add API-requests for follow-unfollow actions

--- src/api/api.js
+++ src/api/api.js
@@ -3,19 +3,50 @@ import * as axios from 'axios';
 export const getUsers = (currentPage = 1, pageSize = 10) => {
     return axios.get(
         `https://social-network.samuraijs.com/api/1.0/users?page=${currentPage}&count=${pageSize}`,
-        {withCredentials: true}
+        { withCredentials: true }
     )
-    .then(response => {
-        return response.data;
-    });
+        .then(response => {
+            return response.data;
+        });
 }
 
 export const checkAuth = () => {
     return axios.get(
         `https://social-network.samuraijs.com/api/1.0/auth/me`,
-        {withCredentials: true}
+        { withCredentials: true }
     )
-    .then(response => {
-        return response.data;
-    });
+        .then(response => {
+            return response.data;
+        });
+}
+
+export const unfollowRequest = (userId) => {
+    return axios.delete(
+        `https://social-network.samuraijs.com/api/1.0/follow/${userId}`,
+        {
+            withCredentials: true,
+            headers: {
+                'API-KEY': '47af2b70-ebd7-4cd1-84e3-1eb690b843d7'
+            }
+        }
+    )
+        .then(response => {
+            return response.data;
+        });
+}
+
+export const followRequest = (userId) => {
+    return axios.post(
+        `https://social-network.samuraijs.com/api/1.0/follow/${userId}`,
+        {},
+        {
+            withCredentials: true,
+            headers: {
+                'API-KEY': '47af2b70-ebd7-4cd1-84e3-1eb690b843d7'
+            }
+        }
+    )
+        .then(response => {
+            return response.data;
+        });
 }
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,7 +1,7 @@
 import s from './Users.module.css';
 import defaultUserIcon from './../../assets/images/defaultUserIcon.png'
 import { NavLink } from 'react-router-dom';
-import * as axios from 'axios';
+import { followRequest, unfollowRequest } from '../../api/api';
 
 let Users = (props) => {
 
@@ -42,34 +42,20 @@ let Users = (props) => {
                                 {
                                     u.followed
                                         ? <button onClick={() => {
-                                            axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`,
-                                                {
-                                                    withCredentials: true,
-                                                    headers: {
-                                                        'API-KEY': '47af2b70-ebd7-4cd1-84e3-1eb690b843d7'
-                                                    }
-                                                })
-                                                .then(response => {
-                                                    if (response.data.resultCode === 0) {
-                                                        props.unfollow(u.id)
-                                                    }
-                                                });
+                                            unfollowRequest(u.id).then(data => {
+                                                if (data.resultCode === 0) {
+                                                    props.unfollow(u.id)
+                                                }
+                                            });
                                         }}>
                                             Unfollow
                                         </button>
                                         : <button onClick={() => {
-                                            axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {},
-                                                {
-                                                    withCredentials: true,
-                                                    headers: {
-                                                        'API-KEY': '47af2b70-ebd7-4cd1-84e3-1eb690b843d7'
-                                                    }
-                                                })
-                                                .then(response => {
-                                                    if (response.data.resultCode === 0) {
-                                                        props.follow(u.id)
-                                                    }
-                                                });
+                                            followRequest(u.id).then(data => {
+                                                if (data.resultCode === 0) {
+                                                    props.follow(u.id)
+                                                }
+                                            });
                                         }}>
                                             Follow
                                         </button>
