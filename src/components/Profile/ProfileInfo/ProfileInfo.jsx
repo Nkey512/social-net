0cf0b7893d3ad25c25a5803d0603e52bc31a1e51
Nikita Kuznetsov <nikita.kuznetsov512@gmail.com>
2021-05-10 03:07:51 +0300

    Add handler for profile Status

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,5 +1,6 @@
 import Preloader from '../../common/Preloader/Preloader';
 import s from './ProfileInfo.module.css'
+import ProfileStatus from './ProfileStatus';
 
 const ProfileInfo = (props) => {
 
@@ -8,21 +9,18 @@ const ProfileInfo = (props) => {
     }
     return (
         <div>
-            <div>
+            {/* <div>
                 <img src='https://cdn.pixabay.com/photo/2021/01/24/20/21/cloud-5946381__340.jpg' />
-            </div>
+            </div> */}
             <div className={s.descriptionBlock}>
                 <div>
                     <img src={props.profile.photos.large} />
+                    <ProfileStatus
+                        status={props.status}
+                        updateStatus={props.updateStatus}/>
                 </div>
                 <div>
                     {props.profile.aboutMe}
-                </div>
-                <div>
-                </div>
-                <div>
-                </div>
-                <div>
                 </div>
             </div>
         </div>
--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -1,21 +1,23 @@
-import { usersAPI } from "../api/api"
+import { profileAPI } from "../api/api"
 
 const ADD_POST = 'ADD_POST'
 const UPDATE_NEW_POST_TEXT = 'UPDATE_NEW_POST_TEXT'
 const SET_USER_PROFILE = 'SET_USER_PROFILE'
+const SET_STATUS = 'SET_STATUS'
 
 let initialState = {
     posts: [
-        {id: 1, message: 'Hi, how are you?', likesCount: 15},
-        {id: 2, message: 'It\'s my first post', likesCount: 23},
+        { id: 1, message: 'Hi, how are you?', likesCount: 15 },
+        { id: 2, message: 'It\'s my first post', likesCount: 23 },
     ],
     newPostText: 'SAMURAI',
-    profile: null
+    profile: null,
+    status: ''
 }
 
 const profileReducer = (statePart = initialState, action) => {
-    
-    switch(action.type) {
+
+    switch (action.type) {
         case ADD_POST:
             let newPost = {
                 id: 3,
@@ -37,22 +39,42 @@ const profileReducer = (statePart = initialState, action) => {
                 ...statePart,
                 profile: action.profile
             }
+        case SET_STATUS:
+            return {
+                ...statePart,
+                status: action.status
+            }
         default:
             return statePart;
     }
 }
 
-export const addPostActionCreator = () => ( {type: ADD_POST} )
+export const addPostActionCreator = () => ({ type: ADD_POST })
 export const updateNewPostTextActionCreator = (text) => ({
     type: UPDATE_NEW_POST_TEXT,
     newText: text
 })
-export const setUserProfile = (profile) => ( {type: SET_USER_PROFILE, profile} )
+export const setUserProfile = (profile) => ({ type: SET_USER_PROFILE, profile })
+export const setStatus = (status) => ({ type: SET_STATUS, status })
 
 // thunkCreators
 export const getUserProfile = (userId) => (dispatch) => {
-    usersAPI.getProfile(userId).then(data => {
+    profileAPI.getProfile(userId).then(data => {
         dispatch(setUserProfile(data));
+    });
+}
+
+export const getStatus = (userId) => (dispatch) => {
+    profileAPI.getStatus(userId).then(data => {
+        dispatch(setStatus(data));
+    });
+}
+
+export const updateStatus = (status) => (dispatch) => {
+    profileAPI.updateStatus(status).then(data => {
+        if (data.resultCode === 0) {
+            dispatch(setStatus(status));
+        }
     });
 }
 
