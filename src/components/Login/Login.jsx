fb9e12a5f3e7c23254ca90eefb84d4ac9d59b521
Nikita Kuznetsov <nikita.kuznetsov512@gmail.com>
2021-05-12 17:02:57 +0300

    Add LoginForm

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,5 +1,29 @@
+const LoginForm = (props) => {
+    return (
+        <form>
+            <div>
+                <input placeholder={"Login"} />
+            </div>
+            <div>
+                <input placeholder={"Password"} />
+            </div>
+            <div>
+                <input type={"checkbox"} /> Remember me
+                </div>
+            <div>
+                <button>Login</button>
+            </div>
+        </form>
+    )
+}
+
 const Login = (props) => {
-    return <h1>LOGIN</h1>
+    return (
+        <div>
+            <h1>LOGIN</h1>
+            <LoginForm />
+        </div>
+    )
 }
 
 export default Login;
